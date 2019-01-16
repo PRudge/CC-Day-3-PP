ad76d56af1ce8cecf21d59c9c19edcd54cef6edd
Pauline Rudge <paulinerudge@me.com>
2019-01-16 15:39:00 +0000

    Test 2 sum arrays

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths( citrus_fruits, berries )
   return citrus_fruits.length + berries.length
 end
+
+def sum_array (numbers)
+total = 0
+  for number in numbers
+    total += number
+  end
+return total
+end  
